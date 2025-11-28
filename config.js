7b30ad1163014e6bb01d5e9dbf46b056d749d134
dcendry1711 <daniel.cendry@gmail.com>
2025-11-28 18:34:08 +0100

    basic styling, mobile layout

--- config.js
+++ config.js
@@ -2,17 +2,13 @@ tailwind.config = {
     theme: {
         extend: {
             colors: {
-            pale_orange: '#ffd9a6',
-            light_orange: '#fbb03b',
-            orange: '#f7931e',
+                paleOrange: "#ffd9a6",
+                lightOrange: "#fbb03b",
+                orange: "#f7931e"
             },
             fontFamily: {
-                sans: ['Josefin Sans', 'sans-serif'],
-                title: ['Playfair Display SC', 'serif']
-            },
-            backgroundImage: {
-                bg_desktop: "url('bg-desktop.png')",
-                bg_mobile: "url('bg-mobile.png')"
+                josefin: ['Josefin Sans', 'sans-serif'],
+                playfair: ['Plaifair Display SC', 'serif'] 
             }
         }
     }
